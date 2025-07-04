f3b07a1028d3ecb06cf54a493cd41ddba1fef2fa
raven22314 <eledesma45955@ucq.edu.mx>
2025-07-04 16:07:32 -0600

    Update Linked List.cpp

--- Topicos avanzados de programacion/Linked List/Linked List/Linked List.cpp
+++ Topicos avanzados de programacion/Linked List/Linked List/Linked List.cpp
@@ -1,10 +1,10 @@
 //Ever Ledesma Anklom 
+
 #include <iostream>
 using namespace std;
 //Recursos Utilizados 
 //https://cplusplus.com/reference/
 //Recursos vistos en clase y asi mismo codigos del profesor
-
 
 
 // clase nodo que guarda un dato y un apuntador al siguiente nodo
@@ -19,14 +19,11 @@ public:
         next = nullptr;
     }
 
-    // constructor de copia 
-    Node(const Node<T>& other) {
-        data = other.data;
-        next = nullptr;
-    }
+   
 };
-
 // clase lista ligada simple
+
+
 template <typename T>
 class LinkedList {
 private:
@@ -39,23 +36,20 @@ public:
         count = 0;
     }
 
-    // 🔻 Destructor agregado 
     ~LinkedList() {
-        clear();  // libera toda la memoria usada
+        clear();
     }
-
-    // inserta un valor al inicio de la lista
+    // clase lista ligada simple
     void push_front(T value) {
         Node<T>* newNode = new Node<T>(value);
         newNode->next = first;
         first = newNode;
         count++;
     }
-
     // elimina el primer nodo de la lista
     void pop_front() {
         if (first == nullptr) {
-            cout << "la lista ya está vacía, no se puede hacer pop_front" << endl;
+            cout << "La lista ya está vacía, no se puede hacer pop_front" << endl;
             return;
         }
         Node<T>* temp = first;
@@ -63,8 +57,9 @@ public:
         delete temp;
         count--;
     }
-
+    
     // imprime la lista de inicio hasta al final 
+
     void Print() {
         Node<T>* current = first;
         while (current != nullptr) {
@@ -73,37 +68,35 @@ public:
         }
         cout << endl;
     }
-
     // función que borra todos los nodos
     void clear() {
         while (first != nullptr) {
-            pop_front(); 
+            pop_front();
         }
     }
-
     // devuelve la cantidad de elementos
     int getCount() {
         return count;
     }
 };
 
-//funcion principal para probar la lista
+//Funcion principal probar la lista
+
 int main() {
+    
     LinkedList<int> lista;
-
-    lista.push_front(10); 
-    lista.push_front(20); 
-    lista.push_front(30); 
-
-    cout << "contenido de la lista: ";
-    lista.Print(); 
-
-    lista.pop_front(); 
-
-    cout << "después de pop_front: ";
-    lista.Print(); 
-
-    lista.clear(); 
-
+    
+    lista.push_front(10);
+    lista.push_front(20);
+    lista.push_front(30);
+    
+    cout << "Contenido de la lista: ";
+    lista.Print();
+    
+    lista.pop_front();
+    cout << "Después de pop_front: ";
+    lista.Print();
+    
+    lista.clear();
     return 0;
 }

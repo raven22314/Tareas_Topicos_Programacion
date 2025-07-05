1609a8eb0cb9e906a57a1356e0bb152a63eadeaa
raven22314 <eledesma45955@ucq.edu.mx>
2025-07-04 21:55:56 -0600

    Update Linked List.cpp

--- Topicos avanzados de programacion/Linked List/Linked List/Linked List.cpp
+++ Topicos avanzados de programacion/Linked List/Linked List/Linked List.cpp
@@ -1,11 +1,90 @@
 //Ever Ledesma Anklom 
-
 #include <iostream>
 using namespace std;
 //Recursos Utilizados 
 //https://cplusplus.com/reference/
 //Recursos vistos en clase y asi mismo codigos del profesor
+//Leer y leer lo que me pedia corregir el profe xd
+//Realmente el rpofesor leera esto?
+//duermanme como a los perritos 
 
+// Clase DynamicArray con conteo de las copias 
+template <typename T>
+class DynamicArray {
+private:
+    T* data;            // Almacena los elementos
+    int size;           // Elementos actuales
+    int capacity;       // Espacio total disponible
+
+#if CONTAR_COPIAS != 0
+
+    int copyCounter;    
+#endif
+
+public:
+    DynamicArray() {
+        capacity = 2;
+        size = 0;
+        data = new T[capacity];
+
+#if CONTAR_COPIAS != 0
+
+        copyCounter = 0;  // Inicializa solo si está activado
+#endif
+    }
+
+    // Destructor
+    ~DynamicArray() {
+        delete[] data;
+    }
+
+    // Devuelve tamaño actual
+    int GetSize() {
+        return size;
+    }
+
+    // Devuelve contador de copias 
+    int GetCopyCounter() {
+#if CONTAR_COPIAS != 0
+
+        return copyCounter;
+#else
+        return -1;
+#endif
+    }
+
+    // Agrega elemento al final
+    void Append(T value) {
+        if (size == capacity) {
+            int newCapacity = capacity * 2;
+            T* newData = new T[newCapacity];
+
+            // Copia elementos incrementando contador si está activado
+            for (int i = 0; i < size; i++) {
+                newData[i] = data[i];
+#if CONTAR_COPIAS != 0
+
+                copyCounter++;
+#endif
+            }
+
+            delete[] data;
+            data = newData;
+            capacity = newCapacity;
+        }
+
+        data[size] = value;
+        size++;
+    }
+
+    // Imprime elementos
+    void Print() {
+        for (int i = 0; i < size; i++) {
+            cout << data[i] << " ";
+        }
+        cout << endl;
+    }
+};
 
 // clase nodo que guarda un dato y un apuntador al siguiente nodo
 template <typename T>
@@ -18,12 +97,9 @@ public:
         data = value;
         next = nullptr;
     }
-
-   
 };
-// clase lista ligada simple
 
-
+// Clase lista ligada
 template <typename T>
 class LinkedList {
 private:
@@ -39,14 +115,14 @@ public:
     ~LinkedList() {
         clear();
     }
-    // clase lista ligada simple
+
     void push_front(T value) {
         Node<T>* newNode = new Node<T>(value);
         newNode->next = first;
         first = newNode;
         count++;
     }
-    // elimina el primer nodo de la lista
+
     void pop_front() {
         if (first == nullptr) {
             cout << "La lista ya está vacía, no se puede hacer pop_front" << endl;
@@ -57,8 +133,6 @@ public:
         delete temp;
         count--;
     }
-    
-    // imprime la lista de inicio hasta al final 
 
     void Print() {
         Node<T>* current = first;
@@ -68,22 +142,19 @@ public:
         }
         cout << endl;
     }
-    // función que borra todos los nodos
+
     void clear() {
         while (first != nullptr) {
             pop_front();
         }
     }
-    // devuelve la cantidad de elementos
+
     int getCount() {
         return count;
     }
 };
 
-//Funcion principal probar la lista
-
 int main() {
-    
     LinkedList<int> lista;
     
     lista.push_front(10);
@@ -98,5 +169,7 @@ int main() {
     lista.Print();
     
     lista.clear();
+
+
     return 0;
 }

fedad76487c8b19c858a43e328a36c8ed6da072f
raven22314 <eledesma45955@ucq.edu.mx>
2025-08-11 23:14:16 -0600

    creo
    creo

--- Topicos avanzados de programacion/BinarySearchTbuenoCreo/BinarySearchTbuenoCreo/BinarySearchTbuenoCreo.cpp
+++ Topicos avanzados de programacion/BinarySearchTbuenoCreo/BinarySearchTbuenoCreo/BinarySearchTbuenoCreo.cpp
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <binary_search_tree.h>
 #include <iostream>
 #include <stack> // necesario para recorridos iterativos
 #include <cmath> // Required for std::log
---
+++ Topicos avanzados de programacion/BinarySearchTbuenoCreo/BinarySearchTbuenoCreo/BinarySearchTree.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+
+#include <iostream>
+#include <stack> // necesario para recorridos iterativos
+#include <cmath> // Required for std::log
+using namespace std;
+
+
+
+// Funciones inline para evitar múltiples definiciones
+inline double log_base_n(double x, double base);
+
+inline int MinimaAlturaDeArbol(int numeroDeNodos, int maximoDeHijosPorNodo);
+
+template <typename T>
+class BinarySearchTree
+{
+    template <typename T>
+    class TreeNode
+    {
+    public:
+        TreeNode()
+        {
+            parent = leftChild = rightChild = nullptr;
+            data = {};
+        }
+
+        TreeNode(T data)
+        {
+            parent = leftChild = rightChild = nullptr;
+            // this se refiere a exactamente el objeto que est� mandando a llamar la funci�n. 
+            this->data = data;
+        }
+
+        T data;
+        // el padre de este nodo dentro del �rbol. 
+        // En otras palabras, this == parent.leftChild || this == parent.rightChild
+        TreeNode<T>* parent;
+
+        // vector<TreeNode*> children; // si quisi�ramos que tuviera la posibilidad de tener m�s de dos hijos cada nodo.
+        TreeNode<T>* leftChild;
+        TreeNode<T>* rightChild;
+    }; 
+    };
